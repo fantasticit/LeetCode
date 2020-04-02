a924158227e1ec42d6ee7d8973876cca0b917ee3
tingyun <tingyun@pinduoduo.com>
2020-04-02 11:58:37 +0800

    add 只出现一次的数字

---
+++ Easy/0136.只出现一次的数字.js
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=136 lang=javascript
+ *
+ * [136] 只出现一次的数字
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+  return nums.reduce((a, c) => (a ^= c), 0);
+};
+// @lc code=end
