c67b2450f8bc05c6d15557c2f3cf3e58530b8a81
tingyun <tingyun@pinduoduo.com>
2020-04-14 11:15:08 +0800

    add 斐波那契数

---
+++ Easy/0509.斐波那契数.js
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=509 lang=javascript
+ *
+ * [509] 斐波那契数
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function (n) {
+  function f(a, b, n) {
+    if (n < 1) return a;
+    return f(b, a + b, n - 1);
+  }
+
+  return f(0, 1, n);
+};
+// @lc code=end
