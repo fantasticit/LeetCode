421ce39369e34f3814ea04c987da16f536f5d139
tingyun <tingyun@pinduoduo.com>
2020-04-16 10:40:32 +0800

    add 二叉树的前序遍历

---
+++ Medium/0144.二叉树的前序遍历.js
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=144 lang=javascript
+ *
+ * [144] 二叉树的前序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function (root) {
+  if (!root) {
+    return [];
+  }
+
+  let ret = [];
+  let stack = [];
+  stack.push(root);
+
+  while (stack.length) {
+    let node = stack.pop();
+    ret.push(node.val);
+
+    if (node.right) {
+      stack.push(node.right);
+    }
+
+    if (node.left) {
+      stack.push(node.left);
+    }
+  }
+
+  return ret;
+};
+// @lc code=end
