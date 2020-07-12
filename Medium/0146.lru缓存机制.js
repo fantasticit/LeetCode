9edbe18cfe98f0e99155823861e45058c09d2095
tingyun <tingyun@pinduoduo.com>
2020-07-12 17:55:35 +0800

    update LRUCache

--- Medium/0146.lru缓存机制.js
+++ Medium/0146.lru缓存机制.js
@@ -67,10 +67,41 @@ LRUCache.prototype.put = function (key, value) {
   }
 };
 
-/**
- * Your LRUCache object will be instantiated and called as such:
- * var obj = new LRUCache(capacity)
- * var param_1 = obj.get(key)
- * obj.put(key,value)
- */
-// @lc code=end
+// // ES6 中 Map 可以记住键值的插入顺序。利用这个特性，可以使用 Map 实现 LRUCache。
+// /**
+//  * @param {number} capacity
+//  */
+// var LRUCache = function (capacity) {
+//   this.cache = new Map();
+//   this.capacity = capacity;
+// };
+
+// /**
+//  * @param {number} key
+//  * @return {number}
+//  */
+// LRUCache.prototype.get = function (key) {
+//   if (!this.cache.has(key)) {
+//     return -1;
+//   }
+
+//   const value = this.cache.get(key);
+//   this.cache.delete(key);
+//   this.cache.set(key, value);
+//   return value;
+// };
+
+// /**
+//  * @param {number} key
+//  * @param {number} value
+//  * @return {void}
+//  */
+// LRUCache.prototype.put = function (key, value) {
+//   if (this.cache.has(key)) {
+//     this.cache.delete(key);
+//   } else if (this.cache.size === this.capacity) {
+//     this.cache.delete(this.cache.keys().next().value);
+//   }
+
+//   this.cache.set(key, value);
+// };
