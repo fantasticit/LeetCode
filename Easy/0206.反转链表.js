c87360231c0a2e45e3758368d177f9aa681b7ec0
fantasticit <fantasticit@163.com>
2021-03-25 10:13:33 +0800

    fix: 优化反转链表解法

--- Easy/0206.反转链表.js
+++ Easy/0206.反转链表.js
@@ -7,43 +7,26 @@
 // @lc code=start
 /**
  * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
  * }
  */
 /**
  * @param {ListNode} head
  * @return {ListNode}
  */
-var reverseList = function(head) {
-  function ListNode(val) {
-    this.val = val;
-    this.next = null;
+var reverseList = function (head) {
+  let current = head;
+  let prev = null;
+
+  while (current) {
+    let next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
   }
 
-  const vals = [];
-  let node = head;
-
-  while (node) {
-    vals.push(node.val);
-    node = node.next;
-  }
-
-  let len = vals.length;
-  let newNode = null;
-  let newHead = null;
-
-  for (let i = len - 1; i >= 0; i--) {
-    if (!newNode) {
-      newNode = new ListNode(vals[i]);
-      newHead = newNode;
-    } else {
-      newNode.next = new ListNode(vals[i]);
-      newNode = newNode.next;
-    }
-  }
-
-  return newHead;
+  return prev;
 };
 // @lc code=end
